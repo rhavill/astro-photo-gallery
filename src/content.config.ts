de6f720742ddb9cbb89994c9957880358d96e462
Ricardo Havill <rhavill@protonmail.com>
2025-02-02 14:22:11 -0500

    Use photos content collection to display photos for an album page, instead of using "getPhotos" function that was copied from a tutorial.

--- src/content.config.ts
+++ src/content.config.ts
@@ -5,7 +5,7 @@ const albums = defineCollection({
     const modules = await import.meta.glob('./album/**/*', { eager: true });
     const albums = Object.keys(modules).map(
       path => {
-        const matches = path.match(/^\.\/album\/([^\/]+)/)
+        const matches = path.match(/^\.\/album\/([^\/]+)/);
         return {
           id: matches[1],
           displayName: matches[1].split("-").map(word => word.charAt(0).toUpperCase() + word.slice(1) ).join(" "),
@@ -23,20 +23,25 @@ const albums = defineCollection({
 const photos= defineCollection({
   loader: async () => {
     const images = await import.meta.glob('./album/**/*', { eager: true });
-    // const resolvedImages = await Promise.all(
-    //   Object.values(images).map((image) => image().then((mod) => mod.default))
-    // );
-    const photos = Object.keys(images).map(key => ({
-      id: key,
-      ...images[key].default,
-    }));
-    // console.log({photos})
+    const photos = Object.keys(images).map(path => {
+      const matches = path.match(/^\.\/album\/([^\/]+)/);
+      return {
+        id: path,
+        album: matches[1],
+        ...images[path].default,
+      };
+    });
+    // console.log('photos',photos)
     return photos;
   },
-  // schema: z.object({
-  //   id: z.string(),
-  //   displayName: z.string(),
-  // }),
+  schema: z.object({
+    id: z.string(),
+    album: reference("albums"),
+    src: z.string(),
+    width: z.number(),
+    height: z.number(),
+    format: z.string(),
+  }),
 });
 
 export const collections = { albums, photos };
